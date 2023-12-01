167f66c88b31505540dc3581e9b912fe6d951804
YBCS <budha627@gmail.com>
2023-12-01 13:50:19 +0530

    day 1 stuff

--- aoc2023/day1.py
+++ aoc2023/day1.py
@@ -77,54 +77,24 @@ def part2(data):
 		# print(f'final ans is {ans} and input was {word}')
 	print('ans is ', ans)
 
+def solution(data):
+	# jonathan paulson solution
+	# store the digit whenever you find a digit
+
+	# 7pqrstsixteen
+	ans = 0
+	for word in data:
+		digits = []
+		for i, char in enumerate(word):
+			if char.isdigit():
+				digits.append(char)
+			for ind, val in enumerate(["one","two","three","four","five","six","seven","eight","nine"]):
+				if word[i:].startswith(val): # this is very clever
+					digits.append(str(ind+1))
+		ans += int(digits[0] + digits[-1])
+	return ans
 
 
-# def part2(data):
-# 	ans = 0
-# 	nums = "123456789"
-# 	word_map = {
-# 	"one" : "1",
-# 	"two" : "2",
-# 	"three" : "3",
-# 	"four" : "4",
-# 	"five" : "5",
-# 	"six" : "6",
-# 	"seven" : "7",
-# 	"eight" : "8",
-# 	"nine" : "9",
-# 	}
 
-# 	for word in data:
-# 		l = 100 # some big no.
-# 		r = -1
-# 		first = -1
-# 		second = -1
-
-# 		for k, v in word_map.items():
-# 			for i in [k,v]:
-# 				temp = word.find(i)
-# 				if temp != -1:
-# 					if temp < l:
-# 						l = temp
-# 						first = n
-# 					if temp > r:
-# 						r = temp
-# 						second = n
-
-# 		print(f"found l {l} and r {r} {first, second}")
-		
-# 		num = ""
-# 		if first in nums:
-# 			num += first
-# 		else:
-# 			num += word_map[first]
-		
-# 		if second in nums:
-# 			num += second
-# 		else:
-# 			num += word_map[second]
-# 		ans += int(num)
-
-# 		print(f'final ans is {ans}')
 
 print(part2(data))
